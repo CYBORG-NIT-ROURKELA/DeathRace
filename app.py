233581899901ba47a724bb7ca7b6348f5e2c7b81
Kamaljeet_Sahoo <kamaljeet@richpanel.com>
2022-10-22 23:14:31 +0530

    Changes

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
+
+st.set_page_config(layout="wide")
 st.header("Death Race")
-st.markdown("Event By Cyborg")
+st.subheader("Event By Cyborg ❤️")
 
 bg_pattern = """
 <style>
@@ -11,6 +13,9 @@ bg_pattern = """
     opacity: 0.8;
     background-image: radial-gradient(#444cf7 0.5px, #e5e5f7 0.5px);
     background-size: 10px 10px;
+}
+[data-testid="stToolbar"] {
+    display: none;
 }
 [data-testid="stHeader"] {
     background-color: rgba(0,0,0,0);
@@ -30,51 +35,25 @@ details = """
 """
 
 st.markdown(bg_pattern, unsafe_allow_html=True)
-
+details = """
+    <style>
+        .text-center {
+            text-align: center;
+        }
+        #event-details {
+            text-align: center;
+        }
+    </style>
+    <h2>Event Details</h2>
+"""
 col1, col2 = st.columns([2,3])
 with col1:
     st.image('DeathRace.jpg')
 with col2: 
+    st.markdown("<div class='event-details'>"+details+"</div>", unsafe_allow_html=True)
 
-# bootstrap 4 collapse example
-    components.html(
-        """
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-        <div id="accordion">
-        <div class="card">
-            <div class="card-header" id="headingOne">
-            <h5 class="mb-0">
-                <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                Collapsible Group Item #1
-                </button>
-            </h5>
-            </div>
-            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-            <div class="card-body">
-                Collapsible Group Item #1 content
-            </div>
-            </div>
-        </div>
-        <div class="card">
-            <div class="card-header" id="headingTwo">
-            <h5 class="mb-0">
-                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                Collapsible Group Item #2
-                </button>
-            </h5>
-            </div>
-            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-            <div class="card-body">
-                Collapsible Group Item #2 content
-            </div>
-            </div>
-        </div>
-        </div>
-        """,
-        height=600,
-    )
+
+st.markdown("<h2 class='text-center'>PROBLEM STATEMENT LINK <a href='https://drive.google.com/file/d/17hyl0eAVeuoSGZPHqp3aECtBXY5fl1Zv/view?usp=sharing'>HERE</a></h2>", unsafe_allow_html=True)
 
 # import base64
 # def add_bg_from_local(image_file):
